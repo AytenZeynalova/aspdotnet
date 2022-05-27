6082881b69e924183106d9cdc582f4d50ac93bd4
unknown <14rvxpg@code.edu.az>
2022-05-27 14:25:30 +0400

    first commit

---
+++ aspfirst/Controllers/ExamController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspfirst.Controllers
+{
+    public class ExamController1 : Controller
+    {
+        public IActionResult Result(int ? studentID , int year)
+        {
+            return Content(studentID.ToString() + " - " + year.ToString());
+
+            //return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return Content(id.ToString());
+
+        }
+    }
+}
---
+++ aspfirst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspfirst.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    return Content("I'M INDEX PAGE");
+        //}
+
+        public ViewResult About()
+        {
+            var view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+
+        public ViewResult Index()
+        {
+            //var views = new ViewResult();
+            //views.ViewName = "Index";
+            //return views;
+
+            return View();
+        }
+    }
+}
